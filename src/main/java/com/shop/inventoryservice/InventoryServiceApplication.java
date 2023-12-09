dedd94846f429167443068e1212db45efd812dba
Hin <tonghohin77@gmail.com>
2023-12-09 16:42:14 -0400

    Eureka

--- src/main/java/com/shop/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/shop/inventoryservice/InventoryServiceApplication.java
@@ -3,12 +3,14 @@ package com.shop.inventoryservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.shop.inventoryservice.model.Inventory;
 import com.shop.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
